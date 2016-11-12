ac507fbaa5ca9ac0e369584956ae710bb03ae5aa
Oskar Rough <oskarrough@gmail.com>
2016-11-12 17:08:32 +0100

    Fetch 500 images, not 10…

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const request = require('request-promise')
 require('dotenv').config({silent: true})
 
 const {API_KEY, API_SECRET, API_NAME} = process.env
-const url = `https://${API_KEY}:${API_SECRET}@api.cloudinary.com/v1_1/${API_NAME}/resources/image`
+const url = `https://${API_KEY}:${API_SECRET}@api.cloudinary.com/v1_1/${API_NAME}/resources/image/upload?max_results=500`
 
 module.exports = async (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*')
